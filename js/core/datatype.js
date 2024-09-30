bf4ae6bb3c3ea0e9ec91ba97ba6027cdb2151518
matt <javascript.12.03.2024@gmail.com>
2024-09-30 20:39:14 +0100

    Update datatype.js

--- js/core/datatype.js
+++ js/core/datatype.js
@@ -1,10 +1,24 @@
-function datatype(v){
+        function datatype(v){
+        
+              var str   = Object.prototype.toString.call(v);
+              str       = str.slice(8,-1);
+              str       = str.toLowerCase();
+              return str;
+              
+        }//datatype
+        
+        datatype.is   = {};
+        
+        datatype.is.node=function(v){
+        
+              if(v===null || typeof v!='object'){
+                    return false;
+              }
+              if('nodeType' in v){
+                    return true;
+              }
+              return false;
+              
+        }//node
 
-      var str    =  Object.prototype.toString.call(v);
-      var i      =  str.indexOf(' ');
-      str        =  str.slice(i+1,-1);
-      str        =  str.toLowerCase();
-      return str;
-
-}
 
