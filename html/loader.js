b891d9fa133474e1f47df9465b34bd9c0a0be313
matt <javascript.12.03.2024@gmail.com>
2024-12-31 12:42:44 +0000

    Update loader.js

--- html/loader.js
+++ html/loader.js
@@ -1,19 +1,20 @@
   (async()=>{
-    var script=document.currentScript;
-    var par=script.parentNode;
-    var url='https://raw.githubusercontent.com/javascript-2020/libs/main/html/';
-    var src=par.getAttrbiute('src');
-    url+=src;
-    var res=await fetch(url);
-    var txt=await res.text();
-    txt=txt.trim();
-    var div=document.createElement('div');
-    div.setHTMLUnsafe(txt);
-    var root=div.firstElementChild;
-    par.parentNode.replaceChild(root,par);
-    var script=root.getElementsByTagName('script')[0];
-    var nscript=document.createElement('script');
-    nscript.textContent=script.textContent;
-    script.parentNode.replaceChild(nscript,script);
+    
+        var script=document.currentScript;
+        var par=script.parentNode;
+        var url='https://raw.githubusercontent.com/javascript-2020/libs/main/html/';
+        var src=par.getAttrbiute('src');
+        url+=src;
+        var res=await fetch(url);
+        var txt=await res.text();
+        txt=txt.trim();
+        var div=document.createElement('div');
+        div.setHTMLUnsafe(txt);
+        var root=div.firstElementChild;
+        par.parentNode.replaceChild(root,par);
+        var script=root.getElementsByTagName('script')[0];
+        var nscript=document.createElement('script');
+        nscript.textContent=script.textContent;
+        script.parentNode.replaceChild(nscript,script);
     
   })();
