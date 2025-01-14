9e747b19de566420e05b26a232af419e457d3bca
matt <javascript.12.03.2024@gmail.com>
2025-01-14 14:16:23 +0000

    save text

--- js/dom/menumod/menumod.js
+++ js/dom/menumod/menumod.js
@@ -524,7 +524,7 @@
               function focusout(e){
                                                                                 debug('focusout');
                     var focus   = e.relatedTarget;
-                                                                                debug(focus);
+                                                                                console.log(focus);
                     if(chk(focus)){
                                                                                 debug('chk');
                           return;
