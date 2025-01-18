9364805a036a60c35527366a10f878d72a93d7a2
matt <javascript.12.03.2024@gmail.com>
2025-01-18 12:56:57 +0000

    save text

--- html/html-loader.js
+++ html/html-loader.js
@@ -66,7 +66,7 @@
                     if(base){
                           var href    = base.href;
                           var url     = new URL(href);
-                          path        = pathname;
+                          path        = url.pathname;
                     }else{
                           path    = window.parent.location.pathname;
                     }
