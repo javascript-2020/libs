63d20d55240f070922e3910402b0535619c9caf9
matt <javascript.12.03.2024@gmail.com>
2025-02-15 15:30:30 +0000

    Update tiny-dbmod.js

--- js/core/dbmod/tiny-dbmod.js
+++ js/core/dbmod/tiny-dbmod.js
@@ -1,15 +1,17 @@
-        function dbmod(data,name='data'){
-          
-              var resolve,promise=()=>new Promise(res=>resolve=res),db
-              var req               = window.indexedDB.open(name,1)
-              req.onsuccess         = e=>{db=req.result;resolve()}
-              req.onupgradeneeded   = e=>{db=req.result;db.createObjectStore(name,{keyPath:'key'})
-                                          data && req.transaction.objectStore(name).put({key:name,data})
-                                          resolve()}
-              obj.delete            = ()=>(window.indexedDB.deleteDatabase(name).onsuccess=e=>resolve(),promise())
-              obj.list              = async()=>(await window.indexedDB.databases()).forEach((db,i)=>console.log(i,':',db.name,db.version))
-              obj.put               = data=>(db.transaction(name,'readwrite').objectStore(name).put({key:name,data}).onsuccess=e=>resolve(),promise())
-              obj.get               = ()=>(db.transaction(name,'readwrite').objectStore(name).get(name).onsuccess=e=>resolve(e.target.result.data),promise())
-              return promise()
-          
-        }//dbmod
+function dbmod(){
+  
+      var resolve,promise=()=>new Promise(res=>resolve=res),db
+      dbmod.create            = (data,name='data')=>{
+                                      var req               = window.indexedDB.open(name,1)
+                                      req.onsuccess         = e=>{db=req.result;resolve()}
+                                      req.onupgradeneeded   = e=>{db=req.result;db.createObjectStore(name,{keyPath:'key'})
+                                                                  data && req.transaction.objectStore(name).put({key:name,data})
+                                                                  resolve()}
+                                }//create
+      dbmod.delete            = name2=>(window.indexedDB.deleteDatabase(name2||name).onsuccess=e=>resolve(),promise())
+      dbmod.list              = async()=>(await window.indexedDB.databases()).forEach(db=>console.log(db.name,db.version))
+      dbmod.put               = data=>(db.transaction(name,'readwrite').objectStore(name).put({key:name,data}).onsuccess=e=>resolve(),promise())
+      dbmod.get               = ()=>(db.transaction(name,'readwrite').objectStore(name).get(name).onsuccess=e=>resolve(e.target.result.data),promise())
+      return promise()
+  
+}//dbmod
