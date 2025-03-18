bcd16438a465f4376254cebd2ce227a52f66f909
matt <javascript.12.03.2024@gmail.com>
2025-03-18 14:01:49 +0000

    Update parse-url.js

--- github/parse-url.js
+++ github/parse-url.js
@@ -2,61 +2,61 @@
 
 
 
-        if(typeof github=='undefined'){
-              github    = {};
-        }
-        github.parse    = {};
 
-    function parse(url){
-    
-          url   = window.decodeURIComponent(url);
-          if(!url.startsWith('http')){
-                url   = 'https://'+url;
-          }
-          url   = new URL(url);
-          if(url.hostname=='javascript-2020.github.io'){
-                parse['github.io'](url);
-          }
-          if(url.hostname=='github.com'){
-                if(url.pathname.indexOf('blob')!=-1){
-                      parse.file(url);
-                }else 
-                if(url.pathname.indexOf('tree')!=-1){
-                      parse.dir(url);
-                }else{
-                      parse.repo(url);
-                }
-          }
-                                          
-    }//parse
 
+        var parse   = {};
+      
+        parse.github    = function(url){
+        
+              url   = window.decodeURIComponent(url);
+              if(!url.startsWith('http')){
+                    url   = 'https://'+url;
+              }
+              url   = new URL(url);
+              var result;
+              if(url.hostname=='javascript-2020.github.io'){
+                    result    = parse['github.io'](url);
+              }
+              if(url.hostname=='github.com'){
+                    if(url.pathname.indexOf('blob')!=-1){
+                          result    = parse.file(url);
+                    }else 
+                    if(url.pathname.indexOf('tree')!=-1){
+                          result    = parse.dir(url);
+                    }else{
+                          result    = parse.repo(url);
+                    }
+              }
+              return result;
+              
+        }//parse
       
         //  https://javascript-2020.github.io/html-components/log/log.html
-        parse['github.io']=function(url){
+        parse.github.io=function(url){
                                                                   debug('github.io');
               var i         = url.hostname.indexOf('.');
               var owner     = url.hostname.slice(0,i);
               var repo      = url.hostname;
               var branch    = 'main';
               var path      = url.pathname.slice(1);
-              write(owner,repo,branch,path);
+              return {owner,repo,branch,path};
               
         }//github.io
         
         //  https://github.com/javascript-2020/libs
-        parse.repo=function(url){
+        parse.github.repo=function(url){
                                                                   debug('repo');
               var parts     = url.pathname.split('/');
               var owner     = parts.shift();
               var repo      = parts.shift();
               var branch    = 'main';
               var path      = '';
-              write(owner,repo,branch,path);
+              return {owner,repo,branch,path};
               
         }//repo
         
         //  https://github.com/javascript-2020/libs/blob/main/docker/nodejs-min.dockerfile
-        parse.file=function(url){
+        parse.github.file=function(url){
                                                                   debug('file');
               var parts     = url.pathname.split('/');
               parts.shift();
@@ -65,12 +65,12 @@
               parts.shift();
               var branch    = parts.shift();
               var path      = parts.join('/');
-              write(owner,repo,branch,path);
+              return {owner,repo,branch,path};
         
         }//file
         
         //  https://github.com/javascript-2020/libs/tree/main/docker
-        parse.dir=function(url){
+        parse.github.dir=function(url){
                                                                   debug('dir');
               var parts     = url.pathname.split('/');
               var owner     = parts.shift();
@@ -78,10 +78,7 @@
               parts.shift();
               var branch    = parts.shift();
               var path      = parts.join('/');
-              write(owner,repo,branch,path);
+              return {owner,repo,branch,path};
               
         }//dir
-
-
-
 
