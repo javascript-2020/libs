7693a5e24c3cf3151eb5e8ecdd1c332d6d584c69
matt <javascript.12.03.2024@gmail.com>
2025-04-30 15:06:05 +0100

    save text

--- html/html-loader.js
+++ html/html-loader.js
@@ -252,6 +252,7 @@
               }
               catch(err){
                                                                                 console.log('html-loader.api');
+                                                                                console.log(nn);
                                                                                 console.log(repo,path);
                     throw err;
               }
