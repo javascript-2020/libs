f3ec786a27d1d23216d7e23d5eb7ff6d90cfb1f3
matt <javascript.12.03.2024@gmail.com>
2025-05-02 15:46:30 +0100

    save text

--- github/api-fetch-test.js
+++ github/api-fetch-test.js
@@ -3,16 +3,41 @@
 
 (async()=>{
 
-
+        window.parent.coutput.replaceChildren();
+        console.clear();
+        
+        
         var owner     = 'javascript-2020';
         var repo      = 'tmp';
         var branch    = 'main';
-        var path      = 'dir/a.js';
+        var path      = 'dir/myfilea.js';
+
         
         var url       = `https://api.github.com/repos/${owner}/${repo}/git/trees/${branch}?recursive=true`;
         var json      = await fetch(url).then(res=>res.json()).catch(error);
 
         console.json(json);
+
+        var item    = json.tree.find(o=>o.path===path);
+        if(item){
+              if(item.type=='blob'){
+                    var i   = path.lastIndexOf('/');
+                    if(i==-1){
+                          path    = '';
+                    }else{
+                          path    = path.slice(0,i);
+                    }
+              }
+        }
+        
+        if(path){
+              if(path.slice(-1)!='/'){
+                    path   += '/';
+              }
+        }
+        
+        console.log('path : ',path);
+        
         
         function error(err){
         
@@ -20,4 +45,7 @@
               
         }//error
 
+
 })();
+
+
