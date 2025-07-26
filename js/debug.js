446ea3f16a912e6b36d20a7c989267574e73795c
matt <javascript.12.03.2024@gmail.com>
2025-07-26 14:37:30 +0100

    Create debug.js

---
+++ js/debug.js
@@ -0,0 +1,37 @@
+
+
+
+
+        function debug(){
+          
+              if(!df)return;
+              var str   = [...arguments].join(' ');
+              console.log('[ https-file ]',str);
+              
+        }//debug
+        
+        
+        debug.log   = function(){
+          
+              if(!df)return;
+              
+              console.log.apply(console,arguments);
+              
+              if(debug.trace){
+                    console.trace();
+              }
+              delete debug.trace;
+              
+        }//log
+        
+        
+        debug.log.trace   = function(){
+          
+              debug.trace   = true;
+              debug.log.apply(null,arguments);
+              
+        }//trace
+
+
+
+
