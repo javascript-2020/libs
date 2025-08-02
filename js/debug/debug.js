e540aaba62a2ca61f32b98478aa45a9d1c20b7c0
matt <javascript.12.03.2024@gmail.com>
2025-08-02 11:39:15 +0100

    Create debug.js

---
+++ js/debug/debug.js
@@ -0,0 +1,60 @@
+
+
+(()=>{
+
+  var obj    =  debug;
+  
+  //var debug_id='';
+
+//:
+  
+        function debug(){
+          
+              if(!df)return;
+              var str   = [...arguments].join(' ');
+              var id='not set';
+              if(typeof debug_id!='undefined'){
+                    id    = debug_id;
+              }
+              console.log(`[ ${id} ]`,str);
+              
+        }//debug
+        
+        
+        debug.log   = function(){
+          
+              if(!df)return;
+              
+              console.log.apply(console,arguments);
+              
+              if(debug.trace){
+                    console.trace();
+              }
+              delete debug.trace;
+              
+        }//log
+        
+        
+        debug.log.trace   = function(){
+          
+              debug.trace   = true;
+              debug.log.apply(null,arguments);
+              
+        }//trace
+
+
+
+
+  
+    return obj;
+
+  //debug:-
+
+  
+})();
+
+
+
+
+
+
