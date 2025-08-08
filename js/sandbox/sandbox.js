9e8a1f738db0a60cb58a0f80f42d747e22741df2
matt <javascript.12.03.2024@gmail.com>
2025-08-08 14:13:25 +0100

    save file

--- js/sandbox/sandbox.js
+++ js/sandbox/sandbox.js
@@ -9,21 +9,32 @@
         obj.run   = run;
         
         
-        function run(js,{clear,disp_result,console}={}){
+        function run(js,{clear,disp_result,console,ctx}={}){
+
+
+              ctx   ||= {};
+
           
-              (()=>{
-
-                    if(clear){
-                          console.clear();
-                    }
-
-                    var result    = eval(js);
+              eval(`
+              
+                    (()=>{
                     
-                    if(disp_result){
-                          dconsole.log('result :',result);
-                    }
-                    
-              })();
+                          {${Object.keys(ctx).join(',')}}    = ctx;
+                          
+                          if(clear){
+                                console.clear();
+                          }
+      
+                          var result    = eval(js);
+                          
+                          if(disp_result){
+                                dconsole.log('result :',result);
+                          }
+                          
+                    })();
+              
+              `);
+
               
         }//sandbox
 
