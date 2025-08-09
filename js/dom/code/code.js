89d1649fde2efe867c60e2a935c28898472b09fe
matt <javascript.12.03.2024@gmail.com>
2025-08-09 15:23:48 +0100

    save file

--- js/dom/code/code.js
+++ js/dom/code/code.js
@@ -76,10 +76,11 @@
 
               //await load.all(root,'snippet-console');
           
+              var css     = '[snippet-console]';
               var nodes   = $.all(rootnode,'code[snippet-console]');
               var list    = new Array(nodes.length);
               
-              nodes       = nodes.map(async(code,i)=>list[i]    = await snippet_console(code));
+              nodes       = nodes.map(async(code,i)=>list[i]    = await snippet_console(code,css));
               await Promise.all(list);
               
               return list;
