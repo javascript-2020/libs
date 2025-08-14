b116bdc2cc93ac630d993ebae64dea9ac6ec2287
matt <javascript.12.03.2024@gmail.com>
2025-08-14 01:09:24 +0100

    save file

--- google/google-storage/google-storage.js
+++ google/google-storage/google-storage.js
@@ -496,7 +496,7 @@ curl -X POST --data-binary @OBJECT_LOCATION \
                     json.items.forEach(item=>{
                           
                           if(item.name.startsWith(path)){
-                                                                                      //console.log(item);
+                                                                                      console.log(item);
                                 var name    = item.name.slice(path.length);
                                 if(name.startsWith('/')){
                                       name    = name.slice(1);
@@ -514,6 +514,8 @@ curl -X POST --data-binary @OBJECT_LOCATION \
                                 }
                                 
                                 var file    = {};
+                                file.abs    = 
+                                file.path   = 
                                 file.name   = name;
                                 file.type   = type;
                                 file.size   = item.size;
