e3af766b85f989bf58bce403465090e19fc52aba
matt <javascript.12.03.2024@gmail.com>
2025-08-18 02:09:02 +0100

    save file

--- js/sandbox/sandbox.js
+++ js/sandbox/sandbox.js
@@ -56,6 +56,7 @@
               var resolve,promise=new Promise(res=>resolve=res);
               
               var iframe              = document.createElement('iframe');
+              iframe.setAttribute('crossorigin','anonymous');
               iframe.style.cssText    = '';
               iframe.onload           = onload;
               document.body.append(iframe);
