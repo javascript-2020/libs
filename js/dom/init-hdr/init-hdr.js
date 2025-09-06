10e161381251ee9e6e8b524dbebdf84c167517ff
matt <javascript.12.03.2024@gmail.com>
2025-09-06 15:40:29 +0100

    save file

--- js/dom/init-hdr/init-hdr.js
+++ js/dom/init-hdr/init-hdr.js
@@ -1,55 +1,50 @@
 
 
+        var mod   = create();
 
+        function create(name){
 
-        var mod           = {};
-
-
-        var stack         = [];
-        stack.name        = '';
-        stack.ct          = 0;
-        stack.total       = 0;
-        stack.df          = false;
-
-        
-        Object.defineProperty(stack,'add',{
-              get:()=>{
-          
-                    stack.total++;
-                                                                                if(stack.df){
-                                                                                      console.log('add',stack.ct,stack.total);
-                                                                                }
-              },//get
-              set:v=>{
-        
-                    stack.total++;
-                    stack.push(v);
+              var mod         = {};
+              mod.create      = create;
+              mod.stack       = [];
+              var ct          = 0;
+              var total       = 0;
+      
+              
+              Object.defineProperty(stack,'add',{
+                    get:()=>{
+                
+                          total++;
+                                                                                      if(mod.stack.df){
+                                                                                            console.log('add',ct,total);
+                                                                                      }
+                    },//get
+                    set:v=>{
+              
+                          total++;
+                          stack.push(v);
+                          
+                    }//set
+              });
+              
+              Object.defineProperty(stack,'complete',{get:()=>{
+                
+                    ct++;
+                                                                                      if(mod.stack.df){
+                                                                                            console.log('complete',ct,total);
+                                                                                      }
+                    if(ct!=total){
+                          return;
+                    }
                     
-              }//set
-        });
-        
-        Object.defineProperty(stack,'complete',{get:()=>{
-          
-              stack.ct++;
-                                                                                if(stack.df){
-                                                                                      console.log('complete',stack.ct,stack.total);
-                                                                                }
-              if(stack.ct!=stack.total){
-                    return;
-              }
-              
-              stack.ct        = 0;
-              stack.total     = 0;
-              var list        = [...stack];
-              stack.length    = 0;
-              list.forEach(fn=>fn());
-              
-        }});
-
-
-        
-        //  (typeof stack!='undefined' && stack.add)
-        //  (typeof stack!='undefined' && stack.complete)
-        
-        
+                    ct              = 0;
+                    total           = 0;
+                    
+                    var list        = [...stack];
+                    stack.length    = 0;
+                    list.forEach(fn=>fn());
+                    
+              }});
+      
+        }//create
         
