cfd052d8ad11e53b041cceb7aa5defb6f106f936
matt <javascript.12.03.2024@gmail.com>
2025-09-06 13:34:21 +0100

    save file

--- js/core/ext-loader/ext-loader.m.js
+++ js/core/ext-loader/ext-loader.m.js
@@ -1,7 +1,12 @@
 
 
-        await fetch('https://libs.ext-code.com/js/core/ext-loader/ext-loader.js').then(res=>res.text().then(eval));
-        var ext   = window.ext;
+        var js    = await fetch('https://libs.ext-code.com/js/core/ext-loader/ext-loader.js').then(res=>res.text());
+        var ext   = eval(`
+              (()=>{
+                    ${js}
+                    return ext;
+              })();
+        `);
         export {ext};
         
         
