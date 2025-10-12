0529aced80362af75fb547553f6cf96a2adb6e49
matt <javascript.12.03.2024@gmail.com>
2025-10-12 10:36:52 +0100

    save file

---
+++ html/snippet-console/ex/ex1.js
@@ -0,0 +1,23 @@
+
+                        
+        fn();
+
+        a++;
+        
+        
+        function fn(){
+        
+              console.log('myFn');
+              
+              var test    = {
+                    hello:'world',
+                    key:'value',
+                    test:123,
+                    debug:true
+              };
+              
+              console.log(test);
+              
+        }//fn
+        
+            
