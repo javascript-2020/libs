39e44930c8ec60ee355eefdcdfc14819c980b662
matt <javascript.12.03.2024@gmail.com>
2025-10-13 19:57:07 +0100

    save file

--- html/snippet-terminal/ex/nodejs-ex.js
+++ html/snippet-terminal/ex/nodejs-ex.js
@@ -10,20 +10,21 @@
         var path    = '/auto-login/';
         
         //https://github.com/javascript-2020/stackoverflow/tree/main/auto-login
-        
+                                                                                console.log(owner,repo,branch);
         if(path[0]=='/')path    = path.slice(1);
         if(path && path.slice(-1)!='/')path  += '/';
-        
+                                                                                console.log(path);
         var file    = `${path.split('/').filter(Boolean).at(-1)||repo}/`;
         fs.mkdirSync(file);    
         var url     = `https://api.github.com/repos/${owner}/${repo}/git/trees/${branch}?recursive=true`;
         var json    = await fetch(url).then(res=>res.json());
         
-        json.tree.forEach(async item=>{
+        json.tree.forEach(async(item,i)=>{
         
               if(!item.path.startsWith(path))return;
-              
+                                                                                console.log(item);
               var fn    = item.path.slice(path.length);
+                                                                                console.log(fn);
               if(item.type=='tree'){
                     fs.mkdirSync(file+fn);
               }else{
