4519755249f5007d66052687dc5956395ce2dc64
matt <javascript.12.03.2024@gmail.com>
2025-10-28 18:14:18 +0000

    save file

--- js/io/github/github.js
+++ js/io/github/github.js
@@ -1142,6 +1142,7 @@
               token             = get.token(token);
               owner             = get.owner(owner);
               var headers       = get.headers({token});
+                                                                                console.log(repo,token);
               var url           = `https://github.com/${owner}/${repo}/archive/refs/heads/${branch}.zip`;
               //const zipUrl = `https://github.com/${user}/${repo}/archive/refs/heads/${branch}.zip`;
               
