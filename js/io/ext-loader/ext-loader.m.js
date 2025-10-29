21ce188e0a85c3afd9c4abfffe099c6e56b538dd
matt <javascript.12.03.2024@gmail.com>
2025-10-29 20:50:25 +0000

    Update ext-loader.m.js

--- js/io/ext-loader/ext-loader.m.js
+++ js/io/ext-loader/ext-loader.m.js
@@ -1,6 +1,6 @@
 
 
-        var js    = await fetch('https://libs.ext-code.com/js/core/ext-loader/ext-loader.js').then(res=>res.text());
+        var js    = await fetch('https://libs.ext-code.com/js/io/ext-loader/ext-loader.js').then(res=>res.text());
         var ext   = eval(`
               (()=>{
                     ${js}
