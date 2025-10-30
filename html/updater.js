810fe52651ce367c68ab1491a96a4c16b28c523f
matt <javascript.12.03.2024@gmail.com>
2025-10-30 07:50:32 +0000

    save file

--- html/updater.js
+++ html/updater.js
@@ -5,14 +5,33 @@
                                                                                 console.log('libs update');
                                                                                 console.log();
 
+        var url           = 'https://github.com/javascript-2020/libs/blob/main/html/file-nav/html/new-file/v2.0/new-file-v2.0.html';
+        var url           = await navigator.clipboard.readText();
+        var parts         = url.split('/');
+        var path          = parts.slice(7,-3).join('/')+'/';
+        console.log(path);
+        var mod           = parts.at(-3);
+        console.log(mod);
+
+
+
+        return;
+
+
+
+
+
+
         var {ext}         = await import('https://libs.ext-code.com/js/io/ext-loader/ext-loader.m.js');
         var [github]      = await ext.load.libs('js/io/github/github.js');
         github.owner      = 'javascript-2020';
         var repo          = 'libs';
-        var path          = 'html/';
         
-        var mod           = 'file-mod';
+        
+        var path          = 'html/file-nav/html/';
+        var mod           = 'new-file';
                                                                                 console.log(mod);
+        
         var {blob}        = await github.file.load({repo,path:`${path}${mod}/v2.0/${mod}-v2.0.html`});
 
         var {ok,error}    = await github.file.save({repo,path:`${path}${mod}/${mod}.html`,blob});
