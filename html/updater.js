bdacaa17f2831ad05a9067cf215e8f3d863ee79f
matt <javascript.12.03.2024@gmail.com>
2025-10-30 07:57:47 +0000

    save file

--- html/updater.js
+++ html/updater.js
@@ -5,36 +5,47 @@
                                                                                 console.log('libs update');
                                                                                 console.log();
 
-        var url           = 'https://github.com/javascript-2020/libs/blob/main/html/file-nav/html/new-file/v2.0/new-file-v2.0.html';
-        var url           = await navigator.clipboard.readText();
-        var parts         = url.split('/');
-        var path          = parts.slice(7,-3).join('/')+'/';
-        console.log(path);
-        var mod           = parts.at(-3);
-        console.log(mod);
+        var {ext}               = await import('https://libs.ext-code.com/js/io/ext-loader/ext-loader.m.js');
+        var [github]            = await ext.load.libs('js/io/github/github.js');
+        github.owner            = 'javascript-2020';
+        var repo                = 'libs';
 
 
 
-        return;
+        var path                = 'html/file-nav/html/';
+        var mod                 = 'new-file';
 
-
-
-
-
-
-        var {ext}         = await import('https://libs.ext-code.com/js/io/ext-loader/ext-loader.m.js');
-        var [github]      = await ext.load.libs('js/io/github/github.js');
-        github.owner      = 'javascript-2020';
-        var repo          = 'libs';
-        
-        
-        var path          = 'html/file-nav/html/';
-        var mod           = 'new-file';
+        if(1){
+              var url           = 'https://github.com/javascript-2020/libs/blob/main/html/file-nav/html/new-file/v2.0/new-file-v2.0.html';
+              var url           = await navigator.clipboard.readText();
+              var parts         = url.split('/');
+              var path          = parts.slice(7,-3).join('/')+'/';
+              var mod           = parts.at(-3);
+        }
+                                                                                console.log(path);
                                                                                 console.log(mod);
-        
-        var {blob}        = await github.file.load({repo,path:`${path}${mod}/v2.0/${mod}-v2.0.html`});
 
-        var {ok,error}    = await github.file.save({repo,path:`${path}${mod}/${mod}.html`,blob});
+        var src                 = `${path}${mod}/v2.0/${mod}-v2.0.html`;
+        var dest                = `${path}${mod}/${mod}.html`;
+                                                                                console.log(src);
+                                                                                console.log(dest);
+
+        var msg   = `
+        path    : ${path}
+        mod     : ${mod}
+        src     : ${src}
+        dest    : ${dest}
+        `;
+        if(!confirm(msg)){
+                                                                                console.log('aborted');
+              return;
+        }
+                                                                            
+        return;
+        
+        var {blob}              = await github.file.load({repo,path:src});
+        var {ok,error}          = await github.file.save({repo,path:dest,blob});
+        
         if(error){
               console.error(error);
               return;
