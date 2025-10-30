64edb5b817d8bc6acb35430675bb1a7910c17e20
matt <javascript.12.03.2024@gmail.com>
2025-10-30 09:00:18 +0000

    save file

--- html/updater.js
+++ html/updater.js
@@ -40,7 +40,7 @@
               var mod;
               if(!result.file){
                                                                                 console.log('dir');
-                    path                    = parts.slice(0,-2).join('/');
+                    path                    = parts.slice(0,-2).join('/')+'/';
                     mod                     = parts.at(-2);
               }else{
                     if(isver(parts.at(-2))){
