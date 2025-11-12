79e22056373bd511650871341d2c2ef69cc523f9
matt <javascript.12.03.2024@gmail.com>
2025-11-12 08:50:36 +0000

    save file

--- js/io/tiny-unzip/tiny-unzip.m.js
+++ js/io/tiny-unzip/tiny-unzip.m.js
@@ -1,12 +1,7 @@
 
 
         var js    = await fetch('https://libs.ext-code.com/js/io/tiny-unzip/tiny-unzip.js').then(res=>res.text());
-        var zip   = eval(`
-              (()=>{
-                    ${js}
-                    return zip;
-              })();
-        `);
+        var zip   = eval(`${js}(zip)`);
         export {zip};
         
         
