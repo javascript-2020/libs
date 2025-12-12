5ac74df0816ac32cb88e3faf9b6d21635329329f
matt <javascript.12.03.2024@gmail.com>
2025-12-12 10:48:10 +0000

    Create init_hdr.bak1.js

---
+++ js/dom/init-hdr/backup/init_hdr.bak1.js
@@ -0,0 +1,70 @@
+
+
+
+                                                                                console.clear();
+                                                                                console.log('[ init-hdr ]');
+                                                                                console.log();
+                                                                                console.json=v=>console.log(JSON.stringify(v,null,4));
+        var df=false,did='init-hdr',version='v1.0'
+        ;
+
+
+        var ext;
+        var $;
+        var datatype;
+        var menumod;
+        var keydown;
+        var debug;
+
+        var ace;
+        
+        var menu;
+            
+        
+        
+    //:
+
+              
+        async function init(){
+                                                                                debug('init',version);
+              
+              menu      = menumod();
+
+              await mod.auto({ext,$,datatype,keydown,menu,menumod,ace});
+              
+              if(typeof start=='function'){
+                    start();
+              }
+              
+        }//init
+
+
+  //:
+  
+  
+(async()=>{
+
+        mod.stack.add;
+        
+        ({ext}    = await import('https://libs.ext-code.com/js/io/ext-loader/ext-loader.m.js'));
+          
+        var promise   = ext.load.libs(
+              'js/dom/$.js.api',
+              'js/core/datatype.js',
+              'js/dom/menumod/menumod.js',
+              'js/dom/keydown/keydown.js',
+              'js/debug/debug.js',
+        );
+        [$,datatype,menumod,keydown,debug]   = await promise;
+  
+        
+        mod.stack.complete;
+              
+})();
+
+                  
+
+
+
+
+
