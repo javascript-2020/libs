bd06bdde3fd92ab1351d7a09363d8e1bbee3d712
matt <javascript.12.03.2024@gmail.com>
2025-12-15 20:02:51 +0000

    save file

--- js/dom/menumod/menumod.js
+++ js/dom/menumod/menumod.js
@@ -724,6 +724,9 @@ function menumod(){
           padding               : 5px 10px;
   }
   
+  .menu-item
+    {display:flex;align-items:center;padding:5px 10px}
+    
   .menu-sep {
           margin                : 10px 0;
           border-top            : 1px solid lightgray;
