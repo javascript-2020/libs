a6339cd67b0e4ad27070093160676283efbf9ecc
matt <javascript.12.03.2024@gmail.com>
2025-12-16 21:00:51 +0000

    save file

--- js/dom/keydown/keydown.js
+++ js/dom/keydown/keydown.js
@@ -1,117 +1,159 @@
 (()=>{
-
                                                                                   //console.log('keydown');
-                                                                                  
-        keydown.stack   = [];
+  var obj   = {
+        version   : 'v1.0',
+  };
+
+        obj.bubbles   = {};
         
+        var capture         = {};
+        capture.stack       = [];
+        capture.keydown     = kd(capture.stack);
         
-        keydown.initdom   = function(){
+        var bubbles         = {};
+        bubbles.stack       = [];
+        bubbles.keydown     = kd(bubbles.stack);
+
         
-              document.addEventListener('keydown',keydown,true);
-              
+  //:
+
+  
+        obj.initdom   = function(){
+        
+              document.addEventListener('keydown',capture.keydown,true);
+              document.addEventListener('keydown',bubbles.keydown,false);
               
         }//initdom
         
         
-        keydown.add   = function(fn){
+        obj.add   = function(fn){
         
-              var i   = keydown.stack.indexOf(fn);
+              var i   = capture.stack.indexOf(fn);
               if(i!=-1)return;
               
               keydown.focus   = true;
               //keydown.stack.push(fn)
-              keydown.stack.unshift(fn);
+              capture.stack.unshift(fn);
               
         }//add
         
 
-        keydown.rem   = function(fn){
+        obj.rem   = function(fn){
         
-              var i   = keydown.stack.indexOf(fn);
+              var i   = capture.stack.indexOf(fn);
               if(i==-1)return;
-              keydown.stack.splice(i,1);
+              capture.stack.splice(i,1);
               
         }//rem
         
 
-        keydown.pop   = function(){
+        obj.pop   = function(){
         
-              keydown.stack.pop()
+              capture.stack.pop()
               
         }//pop
+        
+        
+        
+        obj.bubbles.add   = function(fn){
+          
+              var i   = bubbles.stack.indexOf(fn);
+              if(i==-1)return;
+              
+              bubbles.stack.unshift(fn);
+              
+        }//add
+        
+        
+        obj.bubbles.rem   = function(fn){
+          
+              var i   = bubbles.stack.indexOf(fn);
+              if(i==-1)return;
+              bubbles.stack.splice(i);
+              
+        }//rem
+
 
         
-        function keydown(e){
+        
+        function kd(stack){
+          
+              return keydown;
+              
+              
+              function(e){
                                                                                 console.log('keydown');
                                                                                 console.dir(keydown.stack);
                                                                                 var str   = 'keydown';
-              var stop      = false;
-              var both      = false;
-              var prevent   = false;
-              
-              var result;
-              var fn;
-              
-              var n   = keydown.stack.length;
-              if(n==0){
-                    return;
-              }
-              
-              var i   = n-1;
-              while(i!==null){
-              
-                    fn        = keydown.stack[i];
-                    result    = fn(e);
+                    var stop      = false;
+                    var both      = false;
+                    var prevent   = false;
                     
-                    if(datatype(result)=='object'){
-                          break;
-                    }
-                    if(result===false){
-                          break;
+                    var result;
+                    var fn;
+                    
+                    var n   = stack.length;
+                    if(n==0){
+                          return;
                     }
                     
-                    i   = prev();
+                    var i   = n-1;
+                    while(i!==null){
                     
-              }//while
-              
-              
-              if(result){
-                    ({stop,prevent,both}   = result);
-              }
-                                                                                //console.log(str,stop,prevent,both);
-              if(stop || both){
-                    e.stopPropagation();
-              }
-              if(prevent || both){
-                    e.preventDefault();
-              }
-
-              
-              function prev(){
-
-                    var n   = keydown.stack.length;
-                    for(var j=0;j<n;j++){
-                    
-                          if(keydown.stack[j]===fn){
-                                if(j==0){
-                                      return null;
-                                }
-                                return j-1;
+                          fn        = stack[i];
+                          result    = fn(e);
+                          
+                          if(datatype(result)=='object'){
+                                break;
                           }
-                                
-                    }//for
-                    return null;
+                          if(result===false){
+                                break;
+                          }
+                          
+                          i   = prev();
+                          
+                    }//while
                     
-              }//prev
+                    
+                    if(result){
+                          ({stop,prevent,both}   = result);
+                    }
+                                                                                      //console.log(str,stop,prevent,both);
+                    if(stop || both){
+                          e.stopPropagation();
+                    }
+                    if(prevent || both){
+                          e.preventDefault();
+                    }
+      
+                    
+                    function prev(){
+      
+                          var n   = stack.length;
+                          for(var j=0;j<n;j++){
+                          
+                                if(stack[j]===fn){
+                                      if(j==0){
+                                            return null;
+                                      }
+                                      return j-1;
+                                }
+                                      
+                          }//for
+                          return null;
+                          
+                    }//prev
+                    
+              }//keydown
               
-        }//keydown
+        }//kd
 
 
         
         function datatype(v){return Object.prototype.toString.call(v).slice(8,-1).toLowerCase()}
 
 
-        return keydown;
+  return obj;
 
 })();
 
