138b9242f8b3ec150a2d8ccaebc00525f85cfe6b
matt <javascript.12.03.2024@gmail.com>
2026-01-06 22:34:38 +0000

    save file

--- js/debug/debug.js
+++ js/debug/debug.js
@@ -3,32 +3,36 @@
 (()=>{
 
   var obj    =  debug;
-
-
-  //:
-
   
+  
+  //:
+  
+        if(typeof did=-'undefined'){
+              did   = '???';
+        }
+        
+        
         function debug(...args){
-          
+        
               if(!df && !obj.df)return;
               args.unshift(`[ ${did} ]`);
-              var fmt     = Array.from({length:args.length}).fill('%O').join(' '); 
+              var fmt     = Array.from({length:args.length}).fill('%O').join(' ');
               var args2   = [fmt].concat(args);
               console.groupCollapsed.apply(console,args2);
               console.trace();
               console.groupEnd();
               
-        }//debug  
+        }//debug
         
         
         debug.log   = function(){debug.apply(null,arguments)}
-
-
-
+        
+        
+        
     return obj;
-
+    
   //debug:-
-
+  
   
 })();
 
