847d1cdd735526e8bc1d181adeb8fda5cb89e09b
matt <javascript.12.03.2024@gmail.com>
2026-01-08 20:55:57 +0000

    save file

--- html/snippet-console/ex/ex2.js
+++ html/snippet-console/ex/ex2.js
@@ -1,14 +1,24 @@
-let t = 0;
-setInterval(() => {
-  console.clear();
-  let s = "";
-  for (let y = 0; y < 20; y++) {
-    for (let x = 0; x < 40; x++) {
-      const v = Math.sin((x + t) * 0.2) + Math.cos((y - t) * 0.2);
-      s += v > 1 ? "#" : v > 0 ? "*" : v > -1 ? "." : " ";
-    }
-    s += "\n";
-  }
-  console.log(s);
-  t += 0.3;
-}, 50);
+
+
+        var t = 0;
+        setInterval(()=>{
+        
+              console.clear();
+              var s   = '';
+              for(var y=0;y<20;y++){
+              
+                    for(var x=0;x<40;x++){
+                    
+                          var v   = Math.sin((x+t)*0.2)+Math.cos((y-t)*0.2);
+                          s      += v>1 ? '#' : v>0 ? '*' : v>-1 ? '.' : ' ';
+                          
+                    }//for
+                    s  += '\n';
+                    
+              }//for
+              console.log(s);
+              t  += 0.3;
+              
+        },50);
+        
+        
