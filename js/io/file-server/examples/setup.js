7e271fdb9b2869cbe8939f5dfc590ccfd9e988bc
matt <javascript.12.03.2024@gmail.com>
2026-02-21 22:34:01 +0000

    save file

---
+++ js/io/file-server/examples/setup.js
@@ -0,0 +1,11 @@
+
+
+
+        var {'file-server':fs}    = await import('https://libs.ext-code.com/js/io/file-server/file-server.m.js');
+        console.log(fs[Symbol.toStringTag],':',fs.version);
+        
+        
+        
+        
+        
+        
