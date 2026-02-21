82e1e4f35c4408b2aa59654be75b76bf5b32b2ba
matt <javascript.12.03.2024@gmail.com>
2026-02-21 21:49:51 +0000

    save file

---
+++ js/io/file-server/file-server.m.js
@@ -0,0 +1,16 @@
+
+
+        var js    = await fetch('https://libs.ext-code.com/js/io/file-server/file-server.js').then(res=>res.text());
+        var file_server   = eval(`
+              (()=>{
+                    var mod   = ${js}
+                    return mod;
+              })();
+        `);
+        export {file_server};
+        
+        
+        
+        
+        
+        
