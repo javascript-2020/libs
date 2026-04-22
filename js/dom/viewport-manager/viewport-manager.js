8f46616ab8e52268611b2e9c80839875aaeeda9c
matt <javascript.12.03.2024@gmail.com>
2026-04-22 12:36:28 +0100

    save file

---
+++ js/dom/viewport-manager/viewport-manager.js
@@ -0,0 +1,89 @@
+
+
+
+/*
+
+//viewport-manager:d
+
+22-04-26
+
+
+*/
+
+
+(function(){
+
+  var obj   = {};
+  
+  
+        obj.initmod   = function(params){
+        
+              mod   = params.mod;
+              
+        }//initmod
+        
+        
+  //:
+  
+  
+        var root;
+        
+  //:
+  
+  
+        obj.init    = async function(){
+        }//init
+        
+        
+  //:
+  
+  
+        obj.initdom   = function({par}){
+        
+              par           ||= document.body;
+              
+              root            = document.createElement('div');
+              div.id          = 'viewport-manager';
+              par.append(root);
+              
+              
+        }//initdom
+        
+        
+  //:
+  
+  
+        obj.new   = function({par}){
+        
+              par   ||= root;
+              
+              var node    = document.createElement('view-port');
+              node.toggleAttribute('component',true);
+              par.append(node);
+              
+              var mod2        = mod.create({mod,name:'view-port'});
+              var result      = await mod.build({root:node,mod:mod2});
+              await mod2.auto();
+              
+              var viewport    = mod2['view-port'];
+              viewport.pos({x:200,y:200});
+              viewport.css(`snippet-editor {display:block;height:100%}`);
+              viewport.css(`snippet-editor::part(root) {height:100%}`);
+              
+              return {viewport};
+              
+        }//new
+        
+        
+        obj.tofront   = function(viewport){
+        
+        
+        }//tofront
+        
+        
+        
+        
+  return obj;
+  
+})
+
