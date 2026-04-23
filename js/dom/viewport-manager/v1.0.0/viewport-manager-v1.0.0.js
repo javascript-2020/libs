bcd77c94e8c85686e34aefac4b62727c7e536ca9
matt <javascript.12.03.2024@gmail.com>
2026-04-23 21:23:20 +0100

    save file

---
+++ js/dom/viewport-manager/v1.0.0/viewport-manager-v1.0.0.js
@@ -0,0 +1,142 @@
+
+
+
+/*
+
+//viewport-manager:d
+
+22-04-26
+
+
+*/
+
+
+(function(){
+
+  var obj   = {
+        version   : 'v1.0',
+  };
+  
+  
+        obj.initmod   = function(params){
+        
+              mod   = params.mod;
+              
+        }//initmod
+        
+        
+  //:
+  
+  
+        var root;
+        var list    = [];
+        
+        
+        var front;
+        
+        
+  //:
+  
+  
+        obj.init    = async function(){
+        }//init
+        
+        
+  //:
+  
+  
+        obj.initdom   = function({par}={}){
+        
+              par           ||= document.body;
+              
+              root            = document.createElement('div');
+              root.id         = 'viewport-manager';
+              par.append(root);
+              
+              
+        }//initdom
+        
+        
+  //:
+  
+  
+        obj.new   = async function({par}={}){
+        
+              par           ||= root;
+              
+              var node        = document.createElement('view-port');
+              node.toggleAttribute('component',true);
+              par.append(node);
+              
+              var mod2        = mod.create({mod,name:'view-port'});
+              var result      = await mod.build({root:node,mod:mod2});
+              await mod2.auto();
+              
+              var viewport    = mod2['view-port'];
+              viewport.initmod({vm:obj});
+              //viewport.pos({x:200,y:200});
+              viewport.css(`snippet-editor {display:block;height:100%}`);
+              viewport.css(`snippet-editor::part(root) {height:100%}`);
+              
+              viewport.root.addEventListener('mousedown',md);
+              
+              list.push(viewport);
+              tofront(viewport);
+              
+              return viewport;
+              
+              
+              function md(e){
+              
+                    tofront(viewport);
+                    
+              }//md
+              
+        }//new
+        
+        
+        
+        obj.tofront   = tofront;
+        
+        function tofront(viewport){
+        
+              if(viewport.host.parentNode!==root){
+                    return;
+              }
+              
+              list.forEach(viewport2=>{
+              
+                    if(viewport.host.parentNode===root){
+                          if(viewport2===viewport){
+                                viewport.host.style.zIndex    = list.length;
+                          }else{
+                                var z   = Number(viewport.host.style.zIndex);
+                                z--;
+                                viewport2.host.style.zIndex    = z;
+                          }
+                    }
+                    
+              });
+              front   = viewport;
+              
+        }//tofront
+        
+        
+        obj.remove    = function(viewport){
+        
+              var index   = list.findIndex(viewport2=>viewport2===viewport);
+              if(index==-1){
+                    return;
+              }
+              list.splice(index,1);
+              
+        }//remove
+        
+        
+        
+        
+        
+  return obj;
+  
+})
+
