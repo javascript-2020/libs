f2501f4acfc51958de08a551cc161fb303229177
matt <javascript.12.03.2024@gmail.com>
2026-05-03 22:58:15 +0100

    save file

--- js/crypto/openssl/v2.0.0/openssl-v2.0.0.m.js
+++ js/crypto/openssl/v2.0.0/openssl-v2.0.0.m.js
@@ -1,7 +1,7 @@
 
 
 
-        var txt   = await fetch('https://libs.ext-code.com/js/crypto/openssl/openssl-v2.0.0.js').then(res=>res.text());
+        var txt   = await fetch('https://libs.ext-code.com/js/crypto/openssl/v2.0.0/openssl-v2.0.0.js').then(res=>res.text());
         var js    = `
                     (()=>{
                     
