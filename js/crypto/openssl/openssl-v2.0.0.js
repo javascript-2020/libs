2f3611cae49099479cfe97a04db68b5d3832f095
matt <javascript.12.03.2024@gmail.com>
2026-05-03 22:32:02 +0100

    save file

--- js/crypto/openssl/openssl-v2.0.0.js
+++ js/crypto/openssl/openssl-v2.0.0.js
@@ -254,7 +254,7 @@ function opensslmod(params={}){
         function build_fs(){
         
               for(var key in cur.FS){
-              
+                                                                                console.log(key);
                     obj.fs[key]   = cur.FS[key].bind(cur);
                     
               }//for
