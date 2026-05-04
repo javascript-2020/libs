02d66c1003283d1963e03807021c2a2ed706862a
matt <javascript.12.03.2024@gmail.com>
2026-05-04 01:51:17 +0100

    save file

--- js/crypto/openssl/v2.0.0/openssl.m.js
+++ js/crypto/openssl/v2.0.0/openssl.m.js
@@ -1,9 +1,9 @@
 
 
-        console.log(import.meta.url);
+        console.log('import.meta.url',import.meta.url);
         
         var url   = new URL('./openssl.js', import.meta.url).href;
-        console.log(url);
+        console.log('url',url);
         
         //var txt   = await fetch('https://libs.ext-code.com/js/crypto/openssl/v2.0.0/openssl.js').then(res=>res.text());
         var txt   = await fetch(url).then(res=>res.text());
