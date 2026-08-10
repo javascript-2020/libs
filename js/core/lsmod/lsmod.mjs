2216f1953ee4f9edd6e86f18338dde2f471a3950
matt <javascript.12.03.2024@gmail.com>
2026-08-10 04:08:56 +0100

    save file

--- js/core/lsmod/lsmod.mjs
+++ js/core/lsmod/lsmod.mjs
@@ -3,70 +3,34 @@
 
 /*
 
-openssl.mjs
+lsmod.mjs
 
-27-05-26
+10-08-26
+
 
 */
                                                                                 //debugger;
-                                                                                var df=false;
-                                                                                df && console.log('[ openssl.mjs ]');
-        var platform    = typeof document=='undefined' ? 'node' : 'browser';
-                                                                                df && console.log('[ openssl.mjs ]','       platform  :',platform);
-        var opensslmod;
-        
-        switch(platform){
-        
-          case 'browser'    : await browser();        break;
-          case 'node'       : await node();           break;
-          
-        }//switch
-        
-        export {opensslmod}
-        
-        
-  //:
-  
-  
-        async function browser(){
-                                                                                df && console.log('[ openssl.mjs ]','import.meta.url  :',import.meta.url);
-              var url                   = new URL('./openssl.js',import.meta.url).href;
-                                                                                df && console.log('[ openssl.mjs ]','            url  : ',url);
-              var txt                   = await fetch(url).then(res=>res.text());
-              var js                    = `
+                                                                                var df=false,did='lsmod.mjs';
+                                                                                df && console.log(`[ ${did} ]`);
+                                                                                
+                                                                                df && console.log(`[ ${did} ]`,'import.meta.url  :',import.meta.url);
+        var url                   = new URL('./lsmod.js',import.meta.url).href;
+                                                                                df && console.log(`[ ${did} ]`,'            url  : ',url);
+        var txt                   = await fetch(url).then(res=>res.text());
+        var js                    = `
                                                 (()=>{
                                                 
-                                                      var opensslmod    = ${txt};
-                                                      return opensslmod;
+                                                      var lsmod    = ${txt};
+                                                      return lsmod;
                                                       
                                                 })();
                                           `;
-              opensslmod                = eval(js);
-              
-              var wasmJs                = new URL('openssl.wasm.js',url).href;
-                                                                                df && console.log('[ openssl.mjs ]','wasmJs',wasmJs);
-              opensslmod.wasmJs         = wasmJs;
-              
-        }//browser
+        var lsmod                 = eval(js);
+        
+        export {lsmod}
         
         
-        async function node(){
-                                                                                df && console.log('[ openssl.mjs ]','import.meta.url  :',import.meta.url);
-              var fs                    = await import('node:fs');
-              var path                  = await import('node:path');
-              var {createRequire}       = await import('node:module');
-              var require               = createRequire(import.meta.url);
-              var url                   = await import('node:url');
-              
-              opensslmod                = require('./openssl.js');
-              //opensslmod                = globalThis.opensslmod;
-              
-              opensslmod.fs             = fs;
-              opensslmod.path           = path;
-              opensslmod.__filename     = url.fileURLToPath(import.meta.url);
-              opensslmod.__dirname      = path.dirname(opensslmod.__filename)+'/';
-              
-        }//node
+        
         
         
         
