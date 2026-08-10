5f78dcafefd349f80921a65e146ebf46c7889fd2
matt <javascript.12.03.2024@gmail.com>
2026-08-10 04:11:51 +0100

    save file

--- js/core/ls-mod/ls-mod.mjs
+++ js/core/ls-mod/ls-mod.mjs
@@ -14,7 +14,7 @@ lsmod.mjs
                                                                                 df && console.log(`[ ${did} ]`);
                                                                                 
                                                                                 df && console.log(`[ ${did} ]`,'import.meta.url  :',import.meta.url);
-        var url                   = new URL('./lsmod.js',import.meta.url).href;
+        var url                   = new URL('./ls-mod.js',import.meta.url).href;
                                                                                 df && console.log(`[ ${did} ]`,'            url  : ',url);
         var txt                   = await fetch(url).then(res=>res.text());
         var js                    = `
